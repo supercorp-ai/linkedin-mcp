bd45814880d374cfcc28a9fc0999756327bb6d99
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-03-31 22:38:28 +0100

    Remove unused path

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { SSEServerTransport } from '@modelcontextprotocol/sdk/server/sse.js'
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js'
 import { z } from 'zod'
-import path from 'path'
 import { Redis } from '@upstash/redis'
 
 // --------------------------------------------------------------------
